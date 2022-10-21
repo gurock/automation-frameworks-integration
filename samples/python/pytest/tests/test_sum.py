7e1f60804670b116fc2d4a9b2b25a6281038aa91
d-rede <diogo.v.rede@gmail.com>
2022-10-21 11:46:14 +0100

    Pytest attachments

--- samples/python/pytest/tests/test_sum.py
+++ samples/python/pytest/tests/test_sum.py
@@ -5,13 +5,11 @@ def test_sum_two_numbers():
     assert 1 + 1 == 2
 
 
-def test_sum_two_decimals():
+def test_sum_two_decimals(record_property):
+    record_property("testrail_attachment", "sample_reports/testrail.jpg")
     assert 0.8 + 0.3 == 1.2
 
 
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6)])
 def test_sum_multiple_numbers(test_input, expected):
     assert eval(test_input) == expected
-
-
-
