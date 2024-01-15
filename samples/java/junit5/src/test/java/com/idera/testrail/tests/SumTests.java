394efb74ca914f38eef914b5b989add37097576c
gabriel pita <dvpita@gmail.com>
2024-01-15 17:31:24 -0300

    fix test AddTwoNumbersWithDecimals

--- samples/java/junit5/src/test/java/com/idera/testrail/tests/SumTests.java
+++ samples/java/junit5/src/test/java/com/idera/testrail/tests/SumTests.java
@@ -2,7 +2,6 @@ package com.idera.testrail.tests;
 
 import com.testrail.junit.customjunitxml.TestRailTestReporter;
 import com.testrail.junit.customjunitxml.TestRailTestReporterParameterResolver;
-import com.testrail.junit.customjunitxml.annotations.TestRail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,14 +15,14 @@ class SumTests {
     @Test
     @DisplayName("Add Two Numbers")
     void AddTwoNumbers() {
-        assertEquals(3, 1+2, "1 + 2 should equal 3");
+        assertEquals(3, 1 + 2, "1 + 2 should equal 3");
     }
 
     @Test
     @DisplayName("Add Two Numbers With Decimals")
     void AddTwoNumbersWithDecimals(TestRailTestReporter customReporter) {
         customReporter.setProperty("testrail_attachment", "sample_reports/testrail.jpg");
-        assertEquals(3, 1.5+1.4, "1.5+1.4 should equal 3");
+        assertEquals(2.9, 1.5 + 1.4, "1.5+1.4 should equal 3");
     }
 
     @Nested
@@ -32,7 +31,7 @@ class SumTests {
         @Test
         @DisplayName("Add Three Numbers")
         void AddThreeNumbers() {
-            assertEquals(3, 1+1+1, "1+1+1 should equal 3");
+            assertEquals(3, 1 + 1 + 1, "1+1+1 should equal 3");
         }
     }
 }
