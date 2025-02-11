89f7a203b98870ffb8a4977d382f06a023f2168e
Patrícia Duarte Mateus <patricia.mateus@testrail.com>
2025-02-11 17:57:33 +0000

    Update wdio.conf.js
    updated as suggested.
    To keep it more clear and organized, comment blocks were removed

--- samples/javascript/webdriverIO/wdio.conf.js
+++ samples/javascript/webdriverIO/wdio.conf.js
@@ -15,28 +15,18 @@ export const config = {
         }
     }],
 
-    // ===================
-    // Test Configurations
-    // ===================
-
     logLevel: 'info',
 
     bail: 0,
 
-    // Default timeout for all waitFor* commands.
     waitforTimeout: 1000,
-    //
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
+
     connectionRetryTimeout: 120000,
-    //
-    // Default request retries count
+
     connectionRetryCount: 2,
 
-    // Framework you want to run your specs with.
     framework: 'cucumber',
 
-    // Test reporter for stdout.
     reporters: [
         ['junit', {
             outputDir: './results',
@@ -46,7 +36,6 @@ export const config = {
         }]
     ],
 
-    // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         require: ['./features/step-definitions/steps.js'],
         backtrace: false,
@@ -58,7 +47,6 @@ export const config = {
         source: true,
         strict: false,
         tagExpression: '',
-        //timeout for step definitions
         timeout: 6000,
         ignoreUndefinedDefinitions: false
     },
