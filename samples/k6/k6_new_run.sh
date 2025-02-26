3a175edc35c94e223ae3fb858a3fce25c56f4a90
Patrícia Duarte Mateus <patricia.mateus@testrail.com>
2025-02-26 15:40:54 +0000

    k6 code sample
    first commit

---
+++ samples/k6/k6_new_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+clear
+bold=$(tput bold)
+echo "${bold}----------------------------------------------"
+echo "${bold}--------- K6 TestRail Sample Project ---------"
+echo "${bold}----------------------------------------------"
+echo
+
+echo "${bold}...verify TRCLI is installed"
+echo
+
+trcli
+echo
+
+echo "${bold}...install k6-junit library"
+echo
+
+npm install -g k6
+npm install -g k6-to-junit
+echo
+
+echo "${bold}...execute k6 load test"
+echo
+
+k6 run test_k6.js | k6-to-junit ./reports/junit.xml
+
+echo "${bold}...executing TestRail CLI, uploading results in new test run"
+echo
+
+trcli -y -c "trcli-config.yml" parse_junit -f "./reports/junit.xml" --title "k6 Load Smoke Test - 5 vUser Execution"
+echo
+
+echo "${bold}...execution complete"
---
+++ samples/k6/test_k6.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, group } from 'k6';
+
+const baseUrl = 'http://jsonplaceholder.typicode.com';
+
+export const options = {
+  vus: 5,
+  stages: [
+    { duration: '10s', target: 10 }
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<20'], // 95% of requests should be below 20ms
+    http_reqs: ['count >= 1400', 'count <= 1600'], //total requests generate by k6 should be within 1400 -- 1600
+  },
+};
+
+export default function () {
+  group('JSON Placeholder Performance Testing', function () {
+    group('Posts endpoint', function () {
+      const res = http.get(`${baseUrl}/posts`);
+      check(res, {
+        'is status code 200': (r) => r.status === 200,
+      });
+    });
+  });
+}
+
